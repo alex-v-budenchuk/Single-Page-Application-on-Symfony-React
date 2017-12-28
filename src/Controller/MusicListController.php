7488e9ea5bdd5dfd48ddcf1fb5e43782424e9d1e
blade <buden123@gmail.com>
2017-12-28 20:33:21 +0300

    Backend frame

--- src/Controller/MusicListController.php
+++ src/Controller/MusicListController.php
@@ -2,12 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Track;
 
-class MusicListController
+class MusicListController extends Controller
 {
+    /**
+     * @Route("/")
+     */
     public function index()
     {
-        return new Response('<html><body>test</body></html>');
+        $repository = $this->getDoctrine()->getRepository(Track::class);
+        $tracks = $repository->findAll();
+
+        return $this->render('musiclist/index.html.twig', array('tracks' => $tracks));
     }
+
+
 }
